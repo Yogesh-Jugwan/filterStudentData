9c2da7973735fdb6f59367cb2b2da3eb041ed9d1
Yogesh-Jugwan <yogeshjugwan@gmail.com>
2022-09-06 18:16:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+const studentData = [
+  {
+    studentName: 'A',
+    marks: {
+      "physics": 89,
+      "chemistry": 78,
+      "math": 90
+    },
+  },
+  {
+    studentName: 'B',
+    marks: {
+      "acc": 89,
+      "eco": 78,
+      "math": 90
+    }
+  }
+]
 
 function App() {
+  const [student, setStudent] = useState('');
+  const [subject, setSubject] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={(e) => setStudent(e.target.value)}>
+        <option value={''}></option>
+        {
+          studentData.map((item, index) => {
+            return (<option value={index} key={index}>{item.studentName}</option>)
+          })
+        }
+      </select>
+      <select onChange={(e) => setSubject(e.target.value)}>
+        {student && Object.keys(studentData[student].marks).map(item => {
+          return (<option vlaue={item} key={item}>{item}</option>)
+        })}
+      </select>
+      <p>{subject}: {(student && subject) && studentData[student].marks[subject]}</p>
     </div>
   );
 }
